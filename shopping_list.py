27b91548b2294aa053f674ff9f6528565a176496
Karol Miklasz <karol3wpl@gmail.com>
2023-09-25 21:54:19 +0200

    Add a pharmacy stuff

--- shopping_list.py
+++ shopping_list.py
@@ -1,6 +1,7 @@
 shopping_dict={
     "piekarnia" : ["chleb","bułki","pączek"],
-    "warzywniak" : ["marchew","seler","rukola"]
+    "warzywniak" : ["marchew","seler","rukola"],
+    "apteka" : ["ibuprom", "gripex","cholinex"]
 }
 total_items = 0
 for shop,items in shopping_dict.items():
@@ -8,3 +9,6 @@ for shop,items in shopping_dict.items():
     items_upper = [item.capitalize() for item in items]
     total_items += len(items)
     print(f"Idę do {shop_upper}, kupuję tu następujące rzeczy: {items_upper}.")
+
+    print(f"W sumie kupuję {total_items} produktów.")
+    
