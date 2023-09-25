bb899da95559866f570007daf9ddaac8c204114e
Karol Miklasz <karol3wpl@gmail.com>
2023-09-25 21:59:17 +0200

    Add a sports shop

--- shopping_list.py
+++ shopping_list.py
@@ -1,7 +1,8 @@
 shopping_dict={
     "piekarnia" : ["chleb","bułki","pączek"],
     "warzywniak" : ["marchew","seler","rukola"],
-    "apteka" : ["ibuprom", "gripex","cholinex"]
+    "apteka" : ["ibuprom", "gripex","cholinex"],
+    "sportowy" : ["piłka","bramka","korki"]
 }
 total_items = 0
 for shop,items in shopping_dict.items():
@@ -10,5 +11,5 @@ for shop,items in shopping_dict.items():
     total_items += len(items)
     print(f"Idę do {shop_upper}, kupuję tu następujące rzeczy: {items_upper}.")
 
-    print(f"W sumie kupuję {total_items} produktów.")
+print(f"W sumie kupuję {total_items} produktów.")
     
