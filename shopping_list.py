2fe169dd4f7e04ead851b631a5466ff273c27e15
Karol Miklasz <karol3wpl@gmail.com>
2023-09-25 19:41:04 +0200

    Add for loop and capitalized words

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,9 @@ shopping_dict={
     "piekarnia" : ["chleb","bułki","pączek"],
     "warzywniak" : ["marchew","seler","rukola"]
 }
+total_items = 0
+for shop,items in shopping_dict.items():
+    shop_upper = shop.capitalize()
+    items_upper = [item.capitalize() for item in items]
+    total_items += len(items)
+    print(f"Idę do {shop_upper}, kupuję tu następujące rzeczy: {items_upper}.")
